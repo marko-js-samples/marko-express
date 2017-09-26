3aa1a4884652006e2b1c219fdf99550bdec3b007
Patrick Steele-Idem <pnidem@gmail.com>
2017-09-26 12:18:14 -0700

    Integrated lasso and other cleanup

--- components/app-hello/renderer.js
+++
@@ -1,3 +0,0 @@
-module.exports = function render(input, context) {
-    context.write('Hello ' + input.name + '!');
-};
--- server.js
+++ server.js
@@ -2,13 +2,26 @@ require('marko/node-require').install();
 require('marko/express'); //enable res.marko
 
 var express = require('express');
-var serveStatic = require('serve-static');
 
 var indexTemplate = require('./index.marko');
 var app = express();
 var port = 8080;
 
-app.use('/static', serveStatic(__dirname + '/static'));
+var isProduction = process.env.NODE_ENV === 'production';
+
+// Configure lasso to control how JS/CSS/etc. is delivered to the browser
+require('lasso').configure({
+    plugins: [
+        'lasso-marko' // Allow Marko templates to be compiled and transported to the browser
+    ],
+    outputDir: __dirname + '/static', // Place all generated JS/CSS/etc. files into the "static" dir
+    bundlingEnabled: isProduction, // Only enable bundling in production
+    minify: isProduction, // Only minify JS and CSS code in production
+    fingerprintsEnabled: isProduction, // Only add fingerprints to URLs in production
+});
+
+
+app.use(require('lasso/middleware').serveStatic());
 
 app.get('/', function(req, res) {
     res.marko(indexTemplate, {
